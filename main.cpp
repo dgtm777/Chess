5853ff78fd64a7690897797863346de9a54f8be4
Daria Gitman <dgtm2001@gmail.com>
2019-07-22 13:26:06 +0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+class Figure {
+public:
+	Figure() {}
+	virtual bool check_turn() { return true; }
+};
+
+class Pawn : public Figure {
+public:
+	bool check_turn() {
+		// ...
+	}
+	void become_new_figure() {}
+};
+
+class Horse : public Figure {
+public:
+	bool check_turn() {
+		// ...
+	}
+};
+
+class Chess {
+	void make_turn();
+	void print_field();
+	Figure* field[8][8];
+public:
+	void start_game();
+	Chess() {
+		Figure* pb1 = new Pawn();
+		Figure* pb2 = new Horse();
+		field[0][0] = pb1;
+		field[0][1] = pb2;
+	}
+};
+
+int main()
+{
+
+	return 0;
+}
